b6ed4a26d79201c3cf353706d326de2308d5de75
khánh <25021830@vnu.edu.vn>
2026-05-08 17:11:08 +0700

    dedptrai1

--- src/test/java/com/lab/ShippingCalculatorTest.java
+++ src/test/java/com/lab/ShippingCalculatorTest.java
@@ -25,7 +25,7 @@ public class ShippingCalculatorTest {
   @Test
   void testNullType() {
     // Truyền vào null cho type sẽ gây ra NullPointerException
-    assertThrows(IllegalArgumentException.class, () -> calc.calculate(5, null));
+    assertThrows(IllegalArgumentException.class, () -> calc.calculate(5, "STANDARD"));
   }
   @Test
   void testInvalidWeight() {
